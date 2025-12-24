91a3d7352e581a5b1ca1f53ef9e417f709c92cca
Mahmoud Khairy <mahmoud.khairy@maqta.ae>
2025-12-24 14:30:27 +0400

    End of section 8

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./layout/header/header.component";
+import { HttpClient } from '@angular/common/http';
+import { Pagination } from './shared/models/pagination';
+import { Product } from './shared/models/product';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +11,19 @@ import { HeaderComponent } from "./layout/header/header.component";
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  protected readonly title = 'client';
+
+export class AppComponent implements OnInit {
+  title = 'client';
+  private http = inject(HttpClient);
+  baseUrl = 'https://localhost:4001/api/';
+  products: any[] = [];
+
+  ngOnInit(): void {
+    this.http.get<Pagination<Product>>(this.baseUrl + 'products')
+      .subscribe({
+        next: response => this.products = response.data,
+        error: error => console.error(error),
+        complete: () => console.log('complete')
+      })
+  }
 }
